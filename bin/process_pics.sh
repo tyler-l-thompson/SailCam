68af6dcc6fc7a9a894631faf460c978e4dde97f7
Tyler Thompson <tylerthompson1993@gmail.com>
2019-07-28 23:07:21 -0400

    Update process_pics.sh

--- bin/process_pics.sh
+++ bin/process_pics.sh
@@ -5,15 +5,15 @@ year="${array[4]}"
 month="${array[5]}"
 day="${array[6]}"
 
-mkdir ./rotated
+mkdir ./processed
 for file in *.JPG
 do
-    echo "Rotating: $file"
+    echo "Processing: $file"
     hour="${file:0:2}"
     minute="${file:3:2}"
     second="${file:6:2}"
 
-    convert $file -rotate 90 -gravity SouthEast -pointsize 30 -annotate 0 "$month/$day/$year $hour:$minute:$second" rotated/$file &
+    convert $file -gravity SouthEast -pointsize 30 -fill "#AEAEAE" -annotate 0 "$month/$day/$year $hour:$minute:$second" processed/$file &
 done
 sleep 10
-mencoder mf://rotated/*.JPG -mf w=1944:h=2592:fps=5:type=jpg -ovc lavc -lavcopts vcodec=msmpeg4v2:vbitrate=24000000:keyint=15:mbd=2:trell -oac copy -o "$year-$month-$day.avi"
+mencoder mf://processed/*.JPG -mf w=1944:h=2592:fps=6:type=jpg -ovc lavc -lavcopts vcodec=msmpeg4v2:vbitrate=24000000:keyint=15:mbd=2:trell -oac copy -o "$year-$month-$day.avi"

490e5b91f6d75ec23d81abadf8cb9cdcdb9ce608
Tyler Thompson <tylerthompson1993@gmail.com>
2022-07-07 22:33:02 -0400

    updates

--- bin/generate_timelapse.sh
+++ bin/generate_timelapse.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FPS=30
+FPS=20
 root="$(pwd)"
 IFS='/' read -a array <<< "${root}"
 year="${array[4]}"
@@ -25,8 +25,8 @@ day="${array[6]}"
 # echo "Done."
 echo "Creating timelapse..."
 rm -rf ./$year-$month-$day.avi
-# mencoder mf://*.JPG -mf w=1944:h=2592:fps=$FPS:type=jpg -ovc lavc -lavcopts vcodec=msmpeg4v2:vbitrate=32000000:keyint=15:mbd=2:trell -oac copy -o "$year-$month-$day.avi"
+mencoder mf://*.JPG -mf w=1944:h=2592:fps=$FPS:type=jpg -ovc lavc -lavcopts vcodec=msmpeg4v2:vbitrate=32000000:keyint=15:mbd=2:trell -oac copy -o "$year-$month-$day.avi"
 
-ffmpeg -r $FPS -pattern_type glob -i '*.JPG' -s 1944x2592 -vcodec libx264 -crf 18 -preset slow "$year-$month-$day.avi"
+#ffmpeg -r $FPS -pattern_type glob -i '*.JPG' -s 1944x2592 -vcodec libx264 -crf 18 -preset slow "$year-$month-$day.avi"
 
 echo "Done."
